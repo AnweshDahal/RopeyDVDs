6febe8ee20316dcc5a546c0cb6d91131cd43c55a
PratikAmatya <pratikamatya2@gmail.com>
2022-04-10 22:07:26 +0545

    Added model files for Member, LoanType and MembershipCategory

---
+++ Models/Member.cs
@@ -0,0 +1,13 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string? MemberLastName { get; set; }
+        public string? MemberFirstName { get; set; }
+        public string? MemberAddress { get; set; }
+        public DateTime MemberDOB { get; set; }
+        //foreign key references MemberCategory
+        public int MembershipCategoryNumber { get; set; }
+    }
+}
---
+++ Models/MembershipCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class MembershipCategory
+    {
+        public int Id { get; set; }
+        public string? MembersgipCategoryDescription{ get; set; }
+        public int? MembershipCategoryTotalLoans { get; set; }
+    }
+}
