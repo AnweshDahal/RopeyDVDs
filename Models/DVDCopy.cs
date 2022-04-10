ad53087982c249a1bdb259158947172ec093efb6
samyamdhamala <samyamdhamala11@gmail.com>
2022-04-10 22:17:39 +0545

    3 models

---
+++ Models/DVDCopy.cs
@@ -0,0 +1,9 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class DVDCopy
+    {
+        public int Id { get; set; }
+        public int DVDNumber { get; set; } //fk refrences DVDTitle
+        public DateTime DatePurchased { get; set; }
+    }
+}
---
+++ Models/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int LoanTypeNumber { get; set; } //fk refrences LoanType
+        public int CopyNumber { get; set; } //fk refrences DVDCopy
+        public int MemberNumber { get; set; } //fk refrences Member
+        public DateTime DateOut { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime DateReturned { get; set; }
+    }
+}
