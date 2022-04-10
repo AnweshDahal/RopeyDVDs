0f093a2532c19d4f514c58285c38ab2a5e371e6a
AnweshDahal <anweshd2000@gmail.com>
2022-04-10 22:13:31 +0545

    Add basic Models without references

---
+++ Models/DVDTitle.cs
@@ -0,0 +1,26 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class DVDTitle
+    {
+        public int ID { get; set; }
+
+        // Foreign key references Producer
+        public int ProducerNumber { get; set; }
+
+        // Foreign key references Category
+        public int CategoryNumber { get; set; }
+
+        // Foreign key references Studio
+        public int StudioNumber { get; set; }
+
+        public DateTime DateReleased { get; set; }
+        public float StandardCharge { get; set; }
+        public float PenaltyCharge { get; set; }
+
+        // Define the relationships to other models below for foreign key
+        public DVDTitle()
+        {
+
+        }
+    }
+}
---
+++ Models/Studio.cs
@@ -0,0 +1,12 @@
+﻿namespace RopeyDVDs.Models
+{
+    public class Studio
+    {
+        public int ID { get; set; }
+        public string StudioName { get; set; }
+        public Studio()
+        {
+                
+        }
+    }
+}
