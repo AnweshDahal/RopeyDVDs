346fabb068f8d47427518cc8670855289dea9460
PratikAmatya <pratikamatya2@gmail.com>
2022-04-18 09:18:24 +0545

    Solve conflict

---
+++ DBContext/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RopeyDVDs.Models;
+using RopeyDVDs.Models.ViewModels;
+
+namespace RopeyDVDs.DBContext
+{
+    public class ApplicationDBContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<RopeyDVDs.Models.ViewModels.UserDetailsViewModel> UserDetailsViewModel { get; set; }
+        
+    }
+}
