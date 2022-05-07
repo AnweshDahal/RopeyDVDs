e85cef5f4f85937793cfd31776d7de0502dd3238
Swikarya Dahal <swikarya.dahal305@gmail.com>
2022-05-07 23:43:52 +0545

    RE:Add feature 6

---
+++ Models/ViewModels/IssueLoanViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RopeyDVDs.Models.ViewModels
+{
+    public class IssueLoanViewModel
+    {
+        public int? DVDCopyNumber { get; set; }
+        public int? MemberNumber { get; set; }
+
+        public int? LoanTypeNumber { get; set; }
+    }
+}
---
+++ Models/ViewModels/SaveLoanRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RopeyDVDs.Models.ViewModels
+{
+    public class SaveLoanRequestModel
+    {
+        [Key]
+        public int? ID { get; set; }
+        public int? DVDCopyNumber { get; set; }
+
+        public int? MemberNumber { get; set; }
+
+        public int? LoanTypeNumber { get; set; }
+    }
+}
